652aa98b0b65c1d58f905878bd7df1402bff988a
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-05-02 01:47:59 +0800

    Implement fix for no solution found

--- app/players/tests/player.py
+++ app/players/tests/player.py
@@ -1,6 +1,8 @@
 import random
-from solution.bfs import BFS
-from solution.astar import AStar
+# from solution.bfs import BFS
+# from solution.astar import AStar
+from solution.bfs_improved import BFS
+from solution.greedy import Greedy
 
 class Player():
   name = "testing player"
@@ -11,7 +13,7 @@ class Player():
     ["Hela", "87654654"]
   ]
 
-  # True for A Star, False for Breadth First Search
+  # True for Greedy, False for Breadth First Search
   informed = False
 
   def __init__(self, setup):
@@ -21,16 +23,27 @@ class Player():
     print("SETUP >>>> ", self.setup)
     print("PROBLEM >>>> ", problem)
 
-    # Uninformed Search Solution - Breadth First Search
-    # bfs = BFS(problem['snake_locations'], problem['food_locations'], maze_size=self.setup['maze_size'])
-    # solution, search_tree = bfs.bfs()
+    try:
+      # Uninformed Search Solution - Breadth First Search
+      # bfs = BFS(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
+      # solution, search_tree = bfs.bfs()
 
-    # Informed Search Solution - A Star Pathfinding
-    astar = AStar(problem['snake_locations'], problem['food_locations'], maze_size=self.setup['maze_size'])
-    solution, search_tree = astar.astar()
+      # Informed Search Solution - A Star Pathfinding
+      greedy = Greedy(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
+      solution, search_tree = greedy.greedy()
+    except IndexError:
+      # Catch Index out of Bounds problem
+      # Means that the solution can't be found, and all possible coordinates are explored already
+      # Take the tail as the new goal state to ensure that the snake continues moving / will potentially find a path to the food
+      problem['food_locations'].append(problem['snake_locations'][-1])
+      del problem['snake_locations'][-1]
 
-    # Array of actions (unused, not going to remove just yet)
-    # directions = "nswe"
+      # Uninformed Search Solution - Breadth First Search
+      # bfs = BFS(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
+      # solution, search_tree = bfs.bfs()
 
-    # this function should return the solution and the search_tree
+      # Informed Search Solution - A Star Pathfinding
+      greedy = Greedy(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
+      solution, search_tree = greedy.greedy()
+
     return solution, search_tree
