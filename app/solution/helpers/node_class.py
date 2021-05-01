d10d4253e92c5698c39af3523944592d20f7ccc3
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-05-02 01:48:14 +0800

    Add GreedyNode to node_class.py

--- app/solution/helpers/node_class.py
+++ app/solution/helpers/node_class.py
@@ -13,3 +13,23 @@ class BFSNode:
 
     def addAction(self, action):
         self.actions.append(action)
+
+
+class GreedyNode:
+    def __init__(self, id=None, expansion_sequence=None, state=None, parent=None, g=None, h=None, f=None, removed=False):
+        self.id = id
+        self.expansion_sequence = expansion_sequence
+        self.state = state
+        self.parent = parent
+        self.g = g
+        self.h = h
+        self.f = f
+        self.removed = removed
+        self.children = []
+        self.actions = []
+
+    def addChildren(self, children):
+        self.children.extend(children)
+
+    def addAction(self, action):
+        self.actions.append(action)
