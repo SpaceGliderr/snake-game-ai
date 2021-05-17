f7d599a3fc2d79c9a8f4b97781bae855fcdf496b
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-05-18 07:21:14 +0800

    Code for BFS statistics

--- app/players/bfs/player.py
+++ app/players/bfs/player.py
@@ -14,25 +14,39 @@ class Player():
 
   def __init__(self, setup):
     self.setup = setup
+    self.steps_taken = 0
+    self.nodes_expanded = 0
+    self.rounds = 0
 
   def run(self, problem):
     print("SETUP >>>> ", self.setup)
     print("PROBLEM >>>> ", problem)
+    print("STEPS >>>> ", self.steps_taken)
+    print("NODES_EXPANDED >>>> ", self.nodes_expanded)
+    print("ROUNDS >>>> ", self.rounds)
 
     try:
       # Uninformed Search Solution - Breadth First Search
       bfs = BFS(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
-      solution, search_tree = bfs.bfs()
+      solution, search_tree, expansions = bfs.bfs()
+
     except IndexError:
       # Catch Index out of Bounds problem
       # Means that the solution can't be found, and all possible coordinates are explored already
       # Take the tail as the new goal state to ensure that the snake continues moving / will potentially find a path to the food
       problem['food_locations'].append(problem['snake_locations'][-1])
-      del problem['snake_locations'][-1]
+      k_value = int(len(problem['snake_locations'])/4)
+      # del problem['snake_locations'][-1]
+      res = problem['snake_locations'][: len(problem['snake_locations']) - k_value]
+      print("RESULT >>>> ", res)
 
       # Uninformed Search Solution - Breadth First Search
-      bfs = BFS(problem['snake_locations'], problem['food_locations'], self.setup['maze_size'])
-      solution, search_tree = bfs.bfs()
+      bfs = BFS(res, problem['food_locations'], self.setup['maze_size'])
+      solution, search_tree, expansions = bfs.bfs()
+
+    self.steps_taken += len(solution)
+    self.nodes_expanded += expansions
+    self.rounds += 1
 
     return solution, search_tree
 
